7aa91b62c429c03aeb680559a55e60edb06787bb
Ramakrushna Mishra <ram2mishra@gmail.com>
2022-04-03 11:29:06 +0530

    conig

--- gtag.js
+++ gtag.js
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-P51R3J7ZY5"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M9GXHYXKZR"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-P51R3J7ZY5');
+  gtag('config', 'G-M9GXHYXKZR');
 </script>
